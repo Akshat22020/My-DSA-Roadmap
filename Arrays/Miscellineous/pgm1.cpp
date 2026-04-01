ea8e97824891e164864ea2650ca7a83b37264415
Akshat2 <raj.akshat2202@gmail.com>
2026-04-01 20:25:40 +0530

    Added new array folder for further upgradations

---
+++ Arrays/Miscellineous/pgm1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+
+    int n=0;
+
+    cout<<"Enter size of the array";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter the values of array:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    
+
+}
