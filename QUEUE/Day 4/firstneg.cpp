7b581b6e4d174a1431d83de95b7dc1cd30999197
Akshat2 <raj.akshat2202@gmail.com>
2026-04-23 17:27:42 +0530

    Added queues day 3, 4

---
+++ QUEUE/Day 4/firstneg.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+int main(){
+    int k = 3;
+    int arr [] = { 3 ,-4, -7 , 30 , 7 , -9 , 2 , 1 , 6 , -1};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    vector<int>ans;
+    queue<int>q;
+    for(int i=0;i<n;i++){
+        if(arr[i]<0) q.push(i); // indices of neg elements
+    }
+    int i=0;
+    while(i+k<=n){
+        while(q.size()>0 && q.front()<i) q.pop(); //underflow condn check
+
+        if(q.size()==0||q.front()>=i+k ) ans.push_back(0);
+        else ans.push_back(arr[q.front()]);
+        i++;
+
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+}
